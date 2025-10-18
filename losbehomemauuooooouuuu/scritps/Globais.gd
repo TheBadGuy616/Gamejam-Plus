44da07754628ef2858890174f6c0eb6574d6581d
ALLS04 <andre.siqueira@sou.ufac.br>
2025-10-18 13:29:15 -0300

    Add initial game scripts and scene setup for LosbeHomemAUuOoOoOUUuU

---
+++ losbehomemauuooooouuuu/scritps/Globais.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@export var points = 0
---
+++ losbehomemauuooooouuuu/scritps/Protagonista.gd
@@ -0,0 +1,23 @@
+class_name Player
+extends CharacterBody2D
+
+@export var speed = 400
+@export var tempo = 10
+@export var pontos = 0
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	
+
+
+func _on_timer_timeout() -> void:
+	#get_tree().change_scene_to_file()
+	pass
+	
+func on_enemy_death():
+	
