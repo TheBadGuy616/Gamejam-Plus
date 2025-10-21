5795f51d485db91b410e37a9ff9dbfcd75632f2a
Thiago Costa <draven123.ac123@gmail.com>
2025-10-21 10:24:47 -0500

    feat: agora exibe uma label de Recorde quando você morre com a Pontuação Recorde daquela sessão do jogo

--- losbehomemauuooooouuuu/scritps/Globais.gd
+++ losbehomemauuooooouuuu/scritps/Globais.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var points = 0
+var maxpontos = 0
 
 signal abate_registrado
 var intro_ja_tocou: bool = false
@@ -9,3 +10,8 @@ func registrar_abate_e_pontos(quantidade: int):
 	points += quantidade
 	print("Globais: Pontos atualizados! Total: ", points)
 	abate_registrado.emit()
+
+func update_maxpontos(pontuacao_final: int):
+	if pontuacao_final > maxpontos:
+		maxpontos = pontuacao_final
+	return maxpontos
--- losbehomemauuooooouuuu/scritps/pontuacao_final.gd
+++ losbehomemauuooooouuuu/scritps/pontuacao_final.gd
@@ -1,14 +1,18 @@
 extends Control
 
 @export var pontos: Label
+@onready var maxpontos: Label = $Panel/maxpontos
 
 func _ready():
+	update_max_points_label()
 	update_points_label()
+
+func update_max_points_label():
+	maxpontos.text = str(Globais.update_maxpontos(Globais.points))
 
 func update_points_label():
 	pontos.text = str(Globais.points)
 	Globais.points = 0
-	
 
 func _on_jogar_pressed() -> void:
 	get_tree().change_scene_to_file("res://Personagens/cena_teste.tscn")
